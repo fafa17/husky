975b205b96442b20a5e6f291ffbe9e2ac31fcdce
Chris Liu <cscyliu@comp.polyu.edu.hk>
2016-11-15 11:18:58 +0800

    upload hdfs library

--- parquet/parquet_file_assigner.cpp
+++ parquet/parquet_file_assigner.cpp
@@ -2,4 +2,8 @@
 // Created by Chris Liu on 8/11/2016.
 //
 
+#include <map>
 #include "parquet_file_assigner.hpp"
+
+ParquetSplitAssinger::ParquetSplitAssinger(const std::map<int, std::vector<ParquetSplitInfo>> &request_info_mapping)
+        : request_info_mapping(request_info_mapping) {}
--- parquet/parquet_file_assigner.hpp
+++ parquet/parquet_file_assigner.hpp
@@ -1,13 +1,66 @@
 //
 // Reference:
 // https://github.com/Parquet/parquet-mr/blob/master/parquet-hadoop/src/main/java/parquet/hadoop/ParquetInputFormat.java
-//
+// work like java getSplit
 
 #ifndef PROJECT_PARQUET_FILE_ASSIGNER_HPP
 #define PROJECT_PARQUET_FILE_ASSIGNER_HPP
 
+#include <vector>
+#include <string>
+#include <map>
 
-class parquet_file_assigner {
+enum SplitStatus { pending, reading, fail};
+
+struct HDFSBlockInfo{
+    int start_pos;
+    int end_pos;
+    std::string url;
+};
+
+
+//Metadata for row group
+struct RowGroupInfo{
+
+    //Row group may consist of multi hdfs block
+    std::vector<HDFSBlockInfo> hdfs_block_list;
+
+};
+
+
+struct ParquetSplitInfo{
+
+    std::vector<RowGroupInfo> row_group_list;
+    SplitStatus status;
+
+};
+
+
+class ParquetSplitAssinger {
+
+public:
+    ParquetSplitAssinger();
+
+    ParquetSplitAssinger(const std::map<int, std::vector<ParquetSplitInfo>> &request_info_mapping);
+
+    ~ParquetSplitAssinger() = default;
+
+    void master_main_handler();
+    void master_setup_handler();
+    void browse_local(const std::string& url);
+    std::pair<std::string, size_t> answer(std::string& host, std::string& url);
+    /// Return the number of workers who have finished reading the files in
+    /// the given url
+    int get_num_finished(std::string& url);
+    /// Use this when all workers finish reading the files in url
+    void finish_url(std::string& url);
+
+private:
+
+    int num_workers_alive;
+    int num_split_per_job;
+    std::map<int, std::vector<ParquetSplitInfo>> request_info_mapping;
+
 
 };
 
